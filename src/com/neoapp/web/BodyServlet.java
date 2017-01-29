74b3ba3632273d12fe472cc115c74be771979796
anushabobbala <anushabobbala@gmail.com>
2017-01-29 15:45:46 +0530

    BodyServlet

---
+++ src/com/neoapp/web/BodyServlet.java
@@ -0,0 +1,37 @@
+package com.neoapp.web;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class BodyServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException
+    {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      out.println("<HTML>");
+      out.println("<HEAD><TITLE> A Title to this body</TITLE></HEAD>");
+      out.println("<BODY>");
+
+      RequestDispatcher rd1 =request.getRequestDispatcher("../neoapp/header");
+      rd1.include(request,response);
+
+      out.println("<H1> I'm the Body of this Page </H1>");
+
+      RequestDispatcher rd2 =request.getRequestDispatcher("../neoapp/fotter");
+      rd2.include(request,response);
+
+      out.println("</BODY> </HTML>");
+    }
+}
+
+
+
+
+
+
