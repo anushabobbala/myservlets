5ff10e457c37fe96d5190b0da3b185b35a6ae65f
anushabobbala <anushabobbala@gmail.com>
2017-01-29 15:48:11 +0530

    HeaderServlet

---
+++ src/com/neoapp/web/HeaderServlet.java
@@ -0,0 +1,18 @@
+package com.neoapp.web;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HeaderServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+     throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.println("<H1> I'm from Header </H1>");
+        out.println("<HR>");
+    }
+}
